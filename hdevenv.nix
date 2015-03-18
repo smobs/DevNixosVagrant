65f65d197ab8fea455ce2f4a8353d0b6c47a8afb
Toby Smyth <tobs169@gmail.com>
2015-03-18 19:39:27 +0000

    cabal

--- hdevenv.nix
+++ hdevenv.nix
@@ -1,11 +1,12 @@
 { pkgs, ... }: with pkgs;
 let
-  inherit (haskellPackages_ghc783_profiling) cabal2nix yesodBin ghcMod;
+  inherit (haskellPackages_ghc783_profiling) cabal2nix cabalInstall yesodBin ghcMod;
 in
 {
   environment.systemPackages = [
     cabal2nix
     ghc.ghc783
+    cabalInstall
     ghcMod
     yesodBin
   ];
